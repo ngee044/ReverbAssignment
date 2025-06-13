fc99c831d6dd998510c95bf0c8f8212b75da926d
ngee044 <38107616+ngee044@users.noreply.github.com>
2025-06-14 02:07:27 +0900

    u: JThreadPool 처리 추가

--- ReverbApp/main.cpp
+++ ReverbApp/main.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <signal.h>
 
 
@@ -14,6 +16,7 @@ auto register_signal(void) -> void;
 auto signal_callback(int32_t signum) -> void;
 
 auto parse_arguments(ArgumentParser& arguments) -> void;
+auto setup_reverb_settings(ReverbSettings& settings) -> void;
 
 float room_size_ = 0.7f;
 float damping_ = 0.5f;
@@ -21,42 +24,138 @@ float wet_level_ = 0.5f;
 float dry_level_ = 0.25f;
 float width_ = 1.0f;
 
+unsigned int thread_min_count_ = 2;
+unsigned int thread_max_count_ = 5;
+
 std::string input_file_ = "";
 std::string output_file_ = "";
+
+std::string input_folder_ = "";
+std::string output_folder_ = "";
+
+bool async_ = true;
+
+class ReverbJob : public juce::ThreadPoolJob
+{
+	public:
+	ReverbJob(juce::File input_file, juce::File output_file, const ReverbSettings& settings)
+		: juce::ThreadPoolJob("ReverbJob"), read_file_(std::move(input_file)), write_file_(std::move(output_file)), settings_(settings) {}
+
+	auto runJob() -> JobStatus override
+	{
+		ReverbProcessor processor;
+		auto [result, error] = processor.process_file(read_file_.getFullPathName().toStdString(), write_file_.getFullPathName().toStdString(), settings_);
+		if (!result)
+		{
+			std::cout << "Error processing file: " << (error ? error.value() : "unknown error") << std::endl;
+		}
+
+		return jobHasFinished;
+	}
+
+	private:
+		juce::File read_file_, write_file_;
+		ReverbSettings settings_;
+};
+
 
 auto main(int argc, char* argv[]) -> int
 {
 	ArgumentParser arguments(argc, argv);
 	parse_arguments(arguments);
 
-	if (input_file_.empty() || output_file_.empty())
+	if (input_folder_.empty() && output_folder_.empty())
 	{
-        std::cout << "usage: ReverbApp --input_file <input.wav> --output_file <output.wav> "
-					 "[--room <v>] [--damp <v>] [--wet <v>] [--dry <v>] [--width <v>]\n";
-		return -1;
+		async_ = false;
 	}
 
-	ReverbSettings settings;
-	settings.room_size_ = room_size_;
-	settings.damping_ = damping_;
-	settings.wet_level_ = wet_level_;
-	settings.dry_level_ = dry_level_;
-	settings.width_ = width_;
-
-	std::cout << "Processing file: " << input_file_ << " to " << output_file_ << std::endl;
-	std::cout << "Settings: "
-		   << "Room Size: " << settings.room_size_ << ", "
-		   << "Damping: " << settings.damping_ << ", "
-		   << "Wet Level: " << settings.wet_level_ << ", "
-		   << "Dry Level: " << settings.dry_level_ << ", "
-		   << "Width: " << settings.width_ << std::endl;
-
-	ReverbProcessor processor;
-	auto [result, error] = processor.process_file(input_file_, output_file_, settings);
-	if (!result)
+	if (async_)
 	{
-		std::cout << "Error processing file: " << (error ? error.value() : "unknown error") << std::endl;
-		return -1;
+		if (input_folder_.empty() || output_folder_.empty())
+		{
+			std::cout << "usage: ReverbApp --input_folder <input_folder> --output_folder <output_folder> "
+						<< "[--room <v>] [--damp <v>] [--wet <v>] [--dry <v>] [--width <v>]\n";
+			return -1;
+		}
+
+		juce::File input_folder(input_folder_);
+		juce::File output_folder(output_folder_);
+
+		if (!input_folder.isDirectory() || (!output_folder.isDirectory()))
+		{
+			std::cout << "Error: Input or output folder is not a valid directory.\n";
+			return -1;
+		}
+
+		juce::Array<juce::File> input_files;
+		input_folder.findChildFiles(input_files, juce::File::TypesOfFileToFind::findFiles, false, "*.wav;*.mp3");
+
+		if (input_files.isEmpty())
+		{
+			std::cout << "No audio files found in the input folder.\n";
+			return -1;
+		}
+
+		ReverbSettings settings;
+		setup_reverb_settings(settings);
+
+		std::cout << "Processing files in: " << input_folder_ << " to " << output_folder_ << std::endl;
+		std::cout << "Settings: "
+			<< "Room Size: " << settings.room_size_ << ", "
+			<< "Damping: " << settings.damping_ << ", "
+			<< "Wet Level: " << settings.wet_level_ << ", "
+			<< "Dry Level: " << settings.dry_level_ << ", "
+			<< "Width: " << settings.width_ << std::endl;
+
+		const unsigned int hardware_threads = std::thread::hardware_concurrency();
+		auto thread_count = std::clamp(hardware_threads, thread_min_count_, thread_max_count_);
+		juce::ThreadPool thread_pool(thread_count);
+
+		for (auto& file : input_files)
+		{
+			juce::File output_file = output_folder.getChildFile(file.getFileNameWithoutExtension() + "_reverb.wav");
+			thread_pool.addJob(new ReverbJob(file, output_file, settings), true);
+		}
+
+		thread_pool.removeAllJobs(true, 30000); // Wait for jobs to finish with a timeout of 30 seconds
+		std::cout << "All files processed.\n";
+		if (thread_pool.getNumJobs() > 0)
+		{
+			std::cout << "Some jobs did not finish in time.\n";
+		}
+		else
+		{
+			std::cout << "All jobs completed successfully.\n";
+		}
+	}
+	else
+	{
+		if (input_file_.empty() || output_file_.empty())
+		{
+			std::cout << "usage: ReverbApp --input_file <input.wav> --output_file <output.wav> "
+						"[--room <v>] [--damp <v>] [--wet <v>] [--dry <v>] [--width <v>]\n";
+			return -1;
+		}
+
+		ReverbSettings settings;
+		setup_reverb_settings(settings);
+
+
+		std::cout << "Processing file: " << input_file_ << " to " << output_file_ << std::endl;
+		std::cout << "Settings: "
+			<< "Room Size: " << settings.room_size_ << ", "
+			<< "Damping: " << settings.damping_ << ", "
+			<< "Wet Level: " << settings.wet_level_ << ", "
+			<< "Dry Level: " << settings.dry_level_ << ", "
+			<< "Width: " << settings.width_ << std::endl;
+
+		ReverbProcessor processor;
+		auto [result, error] = processor.process_file(input_file_, output_file_, settings);
+		if (!result)
+		{
+			std::cout << "Error processing file: " << (error ? error.value() : "unknown error") << std::endl;
+			return -1;
+		}
 	}
 
 	register_signal();
@@ -89,10 +188,30 @@ auto signal_callback(int32_t signum) -> void
 	deregister_signal();
 }
 
+auto setup_reverb_settings(ReverbSettings& settings) -> void
+{
+	settings.room_size_ = room_size_;
+	settings.damping_ = damping_;
+	settings.wet_level_ = wet_level_;
+	settings.dry_level_ = dry_level_;
+	settings.width_ = width_;
+}
 
 auto parse_arguments(ArgumentParser& arguments) -> void
 {
-	auto target = arguments.to_string("--input_file");
+	auto target = arguments.to_string("--input_folder");
+	if (target != std::nullopt)
+	{
+		input_file_ = target.value();
+	}
+
+	target = arguments.to_string("--output_folder");
+	if (target != std::nullopt)
+	{
+		output_file_ = target.value();
+	}
+	
+	target = arguments.to_string("--input_file");
 	if (target != std::nullopt)
 	{
 		input_file_ = target.value();
