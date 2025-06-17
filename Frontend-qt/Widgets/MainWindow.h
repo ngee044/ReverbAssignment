5b2fe28bb8dd90d2805b3ad7a5f2a1c00ce2d05c
ngee044 <38107616+ngee044@users.noreply.github.com>
2025-06-17 17:07:19 +0900

    u: QT Project 수정

--- Frontend-qt/Widgets/MainWindow.h
+++ Frontend-qt/Widgets/MainWindow.h
@@ -2,7 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }  
@@ -11,13 +10,13 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
+	
 public:
-	MainWindow(QWidget *parent = nullptr);
+	explicit MainWindow(QWidget *parent = nullptr);
 	~MainWindow();
 
 private:
 	Ui::MainWindow *ui;
 
 };
-
 #endif
